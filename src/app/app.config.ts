681a52de69faa11883ee1dfe881b447e30f91055
BETO\betor <maramirez10@gmail.com>
2026-05-27 13:08:08 -0600

    updated post css and provideZoneChangeDetection()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection, provideZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
