6b8a4b45406d136f7af78e85e6222d713cd95a2b
BETO\betor <maramirez10@gmail.com>
2026-05-27 12:59:55 -0600

    added folders

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatToolbar } from '@angular/material/toolbar';
+import { NgOptimizedImage } from '@angular/common';
+import { SomeForm, SomeFormDto } from './some-form/some-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatSidenavContainer, MatSidenav, MatSidenavContent, MatNavList, MatListItem, MatToolbar, NgOptimizedImage, SomeForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('dashboard');
+  protected readonly open = open;
+
+  onSubmitForm(data: SomeFormDto) {
+    console.log(data);
+  }
 }
